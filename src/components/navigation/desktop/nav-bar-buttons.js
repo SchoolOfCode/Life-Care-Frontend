ffd3b2721e5897bbf2f57394c68c2d39e88d2a7e
Oliver Loftus <oliverloftus1@gmail.com>
2023-01-10 12:21:52 +0000

    started rendering navigation tabs

--- src/components/navigation/desktop/nav-bar-buttons.js
+++ src/components/navigation/desktop/nav-bar-buttons.js
@@ -1,7 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "../../buttons/login-button";
 import { LogoutButton } from "../../buttons/logout-button";
-import { SignupButton } from "../../buttons/signup-button";
+
 
 export const NavBarButtons = () => {
 	const { isAuthenticated } = useAuth0();
@@ -10,7 +10,6 @@ export const NavBarButtons = () => {
 		<div className="nav-bar__buttons">
 			{!isAuthenticated && (
 				<>
-					<SignupButton />
 					<LoginButton />
 				</>
 			)}
--- src/components/navigation/mobile/mobile-nav-bar-buttons.js
+++ src/components/navigation/mobile/mobile-nav-bar-buttons.js
@@ -1,7 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "../../buttons/login-button";
 import { LogoutButton } from "../../buttons/logout-button";
-import { SignupButton } from "../../buttons/signup-button";
 
 export const MobileNavBarButtons = () => {
 	const { isAuthenticated } = useAuth0();
@@ -10,7 +9,6 @@ export const MobileNavBarButtons = () => {
 		<div className="mobile-nav-bar__buttons">
 			{!isAuthenticated && (
 				<>
-					<SignupButton />
 					<LoginButton />
 				</>
 			)}
