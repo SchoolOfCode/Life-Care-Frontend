09bcb34a35e37f91cbb52746065caf48c6180925
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-12 10:15:43 +0000

    added the Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Dashboard from "../src/components/dashboard/Dashboard";
 import LoginPage from "./pages/LoginPage";
 import { Routes, Route } from "react-router-dom";
+import ClientProfilePage from "./pages/ClientProfilePage"
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
         <Route path="/" element={<LoginPage />}>
           <Route index element={<Dashboard />} />
           <Route path="dashboard" element={<Dashboard />} >
-            <Route path=""/>
+            <Route path="clientProfilePage" element={<ClientProfilePage/>}/>
           </Route>
         </Route>
       </Routes>
--- src/components/buttons/ClientButton.js
+++ src/components/buttons/ClientButton.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function ClientButton() {
+  const navigate = useNavigate()
+
   return (
-    <button>Client</button>
+    <button onClick={()=> navigate('ClientProfilePage')}>Client</button>
   )
 }
