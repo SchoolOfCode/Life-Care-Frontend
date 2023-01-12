47123bfbaae0a9cae52f0088662a17f800fa6399
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-12 09:55:23 +0000

    moved the BrowserRouter to index, and creating the routes for the react-pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import Dashboard from "../src/components/dashboard/Dashboard";
 import LoginPage from "./pages/LoginPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />}>
           <Route index element={<Dashboard />} />
-          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} >
+            <Route path=""/>
+          </Route>
         </Route>
       </Routes>
-    </BrowserRouter>
   );
 }
 
