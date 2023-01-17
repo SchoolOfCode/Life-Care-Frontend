91b7c22fdbb0eea9b981659af3ec884bfa4ae986
Oliver Loftus <oliverloftus1@gmail.com>
2023-01-17 10:46:55 +0000

    patient-page

--- src/components/patient-profile.js
+++ src/components/patient-profile.js
@@ -1,6 +1,3 @@
-import { useParams } from "react-router-dom";
-import useFetch from "../hooks/useFetch";
-
 import {
   Avatar,
   Wrap,
@@ -8,23 +5,24 @@ import {
   Heading,
   Card,
   Container,
+  Center,
 } from "@chakra-ui/react";
-
-
 
 export const PatientProfile = (data) => {
   return (
     <Container className="patient-details">
-      <Wrap>
-        <WrapItem>
-          <Avatar
-            size="2xl"
-            name="Segun Adebayo"
-            src="https://bit.ly/sage-adebayo"
-          />
-        </WrapItem>
-      </Wrap>
-      <Heading>{data.patient_name}</Heading>
+      <Center p="20px">
+        <Wrap>
+          <WrapItem>
+            <Avatar
+              size="2xl"
+              name="Segun Adebayo"
+              src="https://bit.ly/sage-adebayo"
+            />
+          </WrapItem>
+        </Wrap>
+      </Center>
+      <Heading key={data.patient_id}>{data.patient_name}</Heading>
       <Card>{data && JSON.stringify(data)}</Card>
     </Container>
   );
--- src/pages/patient-page.js
+++ src/pages/patient-page.js
@@ -1,8 +1,7 @@
-
 import { useParams, useNavigate } from "react-router-dom";
 import { PatientProfile } from "../components/patient-profile";
 import useFetch from "../hooks/useFetch";
-import { Button, Container } from "@chakra-ui/react";
+import { Button, Center, Container } from "@chakra-ui/react";
 
 export const Patient = () => {
   const { id } = useParams();
@@ -12,25 +11,25 @@ export const Patient = () => {
     error,
   } = useFetch(`http://localhost:3005/api/patients/${id}`);
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const navigateToNotes = () => {
-   
     navigate(`/patient/${id}/notes`);
   };
 
   return (
     <Container className="patient-details">
-      <Button
-        onClick={navigateToNotes}
-        colorScheme="teal"
-        size="md"
-        className="tasks-notes"
-      >
-        Tasks & Notes
-      </Button>
+      <Center>
+        <Button
+          onClick={navigateToNotes}
+          colorScheme="teal"
+          size="md"
+          className="tasks-notes"
+        >
+          Tasks & Notes
+        </Button>
+      </Center>
       <PatientProfile patient={patient} />
     </Container>
   );
-
 };
