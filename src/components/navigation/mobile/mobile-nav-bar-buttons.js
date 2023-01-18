46d0b95a0a76fa7d02c4fb238db0a1dc59466689
Steve Beecheno <iamhacktinium@gmail.com>
2023-01-18 11:51:24 +0000

    mobile menu opens. Links currently don't work

--- src/components/navigation/mobile/mobile-nav-bar-buttons.js
+++ src/components/navigation/mobile/mobile-nav-bar-buttons.js
@@ -1,15 +1,33 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { LoginButton } from "../../buttons/login-button";
 import { LogoutButton } from "../../buttons/logout-button";
-import { Box, Button, Center, Flex, IconButton } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { AddIcon, EditIcon, ExternalLinkIcon, HamburgerIcon, RepeatIcon } from "@chakra-ui/icons";
 
 export const MobileNavBarButtons = () => {
 	const { isAuthenticated } = useAuth0();
 
 	return (
 		<Flex display={["flex", "flex", "none", "none"]}>
-			<IconButton colorScheme="blue" aria-label="Search database" icon={<GiHamburgerMenu />} />
+			<Menu>
+				<MenuButton as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" />
+				<MenuList>
+          <MenuItem
+            icon={<AddIcon />}>
+            Dashboard
+          </MenuItem>
+          <MenuItem
+            icon={<ExternalLinkIcon />}>
+            Shifts
+          </MenuItem>
+          <MenuItem
+            icon={<RepeatIcon />}>
+            Admin
+            </MenuItem>
+				</MenuList>
+			</Menu>
+			{/* <IconButton aria-label="Open Menu" icon={<HamburgerIcon />} /> */}
 			{/* <Center>
 				{!isAuthenticated && <LoginButton />}
 				{isAuthenticated && <LogoutButton />}
