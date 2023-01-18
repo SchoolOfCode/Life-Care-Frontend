1e9e436c5047838ace19b1d9bff0b63dbad23bd5
Steve Beecheno <iamhacktinium@gmail.com>
2023-01-18 15:27:17 +0000

    client information is now dynamic

--- src/components/patient-list.js
+++ src/components/patient-list.js
@@ -1,18 +1,9 @@
-import { Badge, Box, Button, Flex, Image, Spacer } from "@chakra-ui/react";
-// import { Navigate } from "react-router-dom";
+import { Box, Button, Flex, Image, Spacer } from "@chakra-ui/react";
 import useFetch from "../hooks/useFetch";
 
 export const PatientList = () => {
-	const dummyImgUrl = "https://images.unsplash.com/photo-1566616213894-2d4e1baee5d8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b2xkJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80";
-
-	const carer_id = 1;
+	const carer_id = 1; //! This needs to be dynamic!
 	const { data: patients, isPending, error } = useFetch(`http://localhost:3005/api/carers/${carer_id}/patients`);
-
-	// const handleClick = (patient) => {
-	// 	Navigate({
-	// 		returnTo: `/patient/${patient.patient_id}`,
-	// 	});
-	// };
 
 	return (
 		<Box mt={133}>
@@ -20,24 +11,21 @@ export const PatientList = () => {
 			{isPending && <div>Loading...</div>}
 			{patients && (
 				<div p="10">
+					{patients && JSON.stringify(patients)}
 					{patients.map((patient) => {
 						return (
 							<Box key={patient.patient_id} boxShadow="xl" maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
-								<Image src={dummyImgUrl} alt={"client"} />
+								<Image src={patient.avatar} alt={"client"} />
 								<Flex>
 									<Box w={"80%"} p="6" bg="white">
 										<Box display="flex" align="baseline">
-											<Badge borderRadius="sm" px="2" colorScheme="teal">
-												New
-											</Badge>
-											<Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase" ml="2">
-												age 72
+											<Box color="gray.500" fontWeight="semibold" letterSpacing="wide" fontSize="xs" textTransform="uppercase">
+												DOB: {patient.dob}
 											</Box>
 										</Box>
 										<Box mt="1" fontWeight="semibold" as="h4" lineHeight="tight" noOfLines={1} fontSize="xl">
-											{patient.patient_name}
+											{patient.first_name} {patient.last_name}
 										</Box>
-										<Box>Female</Box>
 									</Box>
 									<Spacer />
 									<Flex gap={3} direction={"column"} grow={1} p="4" bg="white">
--- src/pages/dashboard-page.js
+++ src/pages/dashboard-page.js
@@ -1,17 +1,17 @@
 import { Center, Container } from "@chakra-ui/react";
 import { PatientList } from "../components/patient-list";
-// import useFetch from "../hooks/useFetch";
+import useFetch from "../hooks/useFetch";
 
 export default function Dashboard() {
-	// const carer_id = 1;
-	// let { data: carer, error, isPending } = useFetch(`http://localhost:3005/api/carers/${carer_id}`);
+	const carer_id = 1;
+	let { data: carer, error, isPending } = useFetch(`http://localhost:3005/api/carers/${carer_id}`);
 
 	return (
 		<Container>
-			<Center>
-				{/* {error && <div>{error}</div>}
+				{error && <div>{error}</div>}
 				{isPending && <div>Loading...</div>}
-				{carer && JSON.stringify(carer)} */}
+				{carer && JSON.stringify(carer)}
+			<Center>
 				<PatientList />
 			</Center>
 		</Container>
