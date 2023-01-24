cef4a87a79662a7e0d8521946ddef5417cb1cce7
Steve Beecheno <iamhacktinium@gmail.com>
2023-01-24 16:37:57 +0000

    logout link added to hamburger menu

--- src/components/navigation/mobile/hamburger-menu.js
+++ src/components/navigation/mobile/hamburger-menu.js
@@ -1,8 +1,17 @@
-import { Flex, IconButton, Link, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { Button, Flex, IconButton, Link, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import { Link as ReactRouterLink } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const HamburgerMenu = () => {
+	const { logout } = useAuth0();
+
+	const handleLogout = () => {
+		logout({
+			returnTo: window.location.origin,
+		});
+	};
+
 	return (
 		<Flex display={["flex", "flex", "none", "none"]}>
 			<Menu>
@@ -17,6 +26,9 @@ export const HamburgerMenu = () => {
 					<Link as={ReactRouterLink} to="#" style={{ textDecoration: "none" }}>
 						<MenuItem>Admin</MenuItem>
 					</Link>
+					<MenuItem as={Button} onClick={handleLogout} style={{ fontWeight: "400", justifyContent: "left" }}>
+						Logout
+					</MenuItem>
 				</MenuList>
 			</Menu>
 		</Flex>
