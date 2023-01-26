475cb807cb059811e75d7fe91e6e18c078a1f0f6
Oliver Loftus <oliverloftus1@gmail.com>
2023-01-26 11:53:51 +0000

    started writing login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,59 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add(
+  "loginByAuth0Api",
+  (username: string, password: string) => {
+    cy.log(`Logging in as ${username}`);
+    const client_id = Cypress.env("REACT_APP_AUTH0_CLIENT_ID");
+    const client_secret = Cypress.env("REACT_APP_AUTH0_CLIENT_SECRET");
+    const audience = Cypress.env("REACT_APP_AUTH0_AUDIENCE");
+
+    cy.request({
+      method: "POST",
+      url: `https://${Cypress.env("REACT_APP_AUTH0_DOMAIN")}/oauth/token`,
+      body: {
+        grant_type: "password",
+        username,
+        password,
+        audience,
+        client_id,
+        client_secret,
+      },
+    }).then(({ body }) => {
+      const claims = jwt.decode(body.id_token);
+      const {
+        nickname,
+        name,
+        picture,
+        updated_at,
+        email,
+        email_verified,
+        sub,
+        exp,
+      } = claims;
+
+      const item = {
+        body: {
+          ...body,
+          decodedToken: {
+            claims,
+            user: {
+              nickname,
+              name,
+              picture,
+              updated_at,
+              email,
+              email_verified,
+              sub,
+            },
+            audience,
+            client_id,
+          },
+        },
+        expiresAt: exp,
+      };
+
+      window.localStorage.setItem("auth0Cypress", JSON.stringify(item));
+
+      cy.visit("/");
+    });
+  }
+);
